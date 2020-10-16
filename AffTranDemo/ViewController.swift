ea704addf7ac129eecce4175ac03dd7808b3ddfa
Jaydeep Joshi <jaydeepmjoshi@gmail.com>
2020-10-16 11:39:07 +0530

    Initial Commit

---
+++ AffTranDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AffTranDemo
+//
+//  Created by jaydeep on 16/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
